c79bc2cc29c4ffbf546eca16478cb63f8c8ac6a9
Eric Smith <esmith@thebrighttag.com>
2017-04-23 11:35:41 -0500

    formatting changes

--- ch02/exercise-2.1.scala
+++ ch02/exercise-2.1.scala
@@ -1,8 +1,8 @@
 def fib(n: Int): Int = {
   @annotation.tailrec
   def go(n: Int, prev: Int, curr: Int): Int = {
-    if (n <= 0) prev
-    else go(n - 1, curr, prev + curr)
+    if (n<=0) prev
+    else go(n-1,curr,prev+curr)
   }
-  go(n, 0, 1)
+  go(n,0,1)
 }
