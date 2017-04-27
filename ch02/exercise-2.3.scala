70de2b48c2f4241928b437f4aac54aee9a9701b1
smittyfest <smittyfest@users.noreply.github.com>
2017-04-27 12:45:56 -0500

    Update exercise-2.3.scala

--- ch02/exercise-2.3.scala
+++ ch02/exercise-2.3.scala
@@ -10,4 +10,6 @@ def partial1[A,B,C](a: A, f: (A,B) => C): B => C = {
  * Polymorphic Higher-Order Function for
  * Currying
  */
- def curry[A,B,C](f: (A,B) => C): A => (B => C)
+ def curry[A,B,C](f: (A,B) => C): A => (B => C) = {
+   (a: A) => (b: B) => f(a, b)
+ }
