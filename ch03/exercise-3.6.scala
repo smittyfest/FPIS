70b46cda8227579c8b021c82d3a294a2b3b09378
smittyfest <smittyfest@users.noreply.github.com>
2017-05-02 13:09:19 -0500

    Update exercise-3.6.scala

--- ch03/exercise-3.6.scala
+++ ch03/exercise-3.6.scala
@@ -6,4 +6,11 @@
    case Cons(h, t) => Cons(h, append(t, second))
  }
  
- 
+/**
+ * Returns all but the last element of a List[A]
+ */
+def init[A](as: List[A]): List[A] = as match {
+  case Nil => Nil
+  case Cons(_, Nil) => Nil
+  case Cons(h, t) => Cons(h, init(t))
+}
